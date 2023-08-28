4915a9510181a2fd157d7831a3799b29e9673ed8
Andrew Zhu <me@andrew-zhu.com>
2023-08-28 13:39:18 -0400

    chore: allow continued conversation with helper

--- kanpai/core.py
+++ kanpai/core.py
@@ -10,10 +10,18 @@ log = logging.getLogger(__name__)
 
 
 class Kanpai(BrowsingKani):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = None
+
     # delegation
     @ai_function()
     async def delegate(
-        self, instructions: Annotated[str, AIParam("Detailed instructions on what your helper should do to help you.")]
+        self,
+        instructions: Annotated[str, AIParam("Detailed instructions on what your helper should do to help you.")],
+        new: Annotated[
+            bool, AIParam("Continue the conversation with the same helper (false) or ask a new helper (true).")
+        ] = False,
     ):
         """
         Ask a capable helper for help looking up a piece of information or performing an action.
@@ -21,13 +29,16 @@ class Kanpai(BrowsingKani):
         into multiple steps.
         """
         log.info(f"Delegated with instructions: {instructions}")
-        helper = Kanpai(self.engine, browser=self.browser, system_prompt=ROOT_KANPAI)
+        # set up the helper
+        if new and self.helper is not None and self.helper.context:
+            # close an existing helper's browser context
+            await self.helper.context.close()
+        if self.helper is None or new:
+            self.helper = Kanpai(self.engine, browser=self.browser, system_prompt=ROOT_KANPAI)
+
         result = []
-        async for msg in helper.full_round(instructions):
+        async for msg in self.helper.full_round(instructions):
             log.info(msg)
             if msg.content:
                 result.append(msg.content)
-        # close the sub-helper's browser context
-        if helper.context:
-            await helper.context.close()
         return "\n".join(result)
