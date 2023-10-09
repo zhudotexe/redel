55b56c7a8d5031840b3e2d03432674aa57a2fb55
Andrew Zhu <me@andrew-zhu.com>
2023-10-09 15:42:54 -0600

    feat: add basic node viz

--- kanpai-viz/src/App.vue
+++ kanpai-viz/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Chat from "@/components/Chat.vue";
+import ChatMessages from "@/components/ChatMessages.vue";
 import Tree from "@/components/Tree.vue";
 import { KanpaiClient } from "@/kanpai/client";
-import { onMounted, onUnmounted, provide, reactive } from "vue";
+import type { KaniState } from "@/kanpai/state";
+import { onMounted, onUnmounted, provide, reactive, ref } from "vue";
 
 const client = reactive(new KanpaiClient());
+const introspectedKani = ref<KaniState | null>(null);
 
 provide("client", client);
 
@@ -29,7 +32,13 @@ onUnmounted(() => client.close());
     </div>
     <!-- viz -->
     <div class="column">
-      <Tree />
+      <div class="right-container">
+        <Tree class="tree" @node-clicked="(id) => (introspectedKani = client.kaniMap.get(id) ?? null)" />
+        <div class="introspection-container">
+          <ChatMessages :kani="introspectedKani" v-if="introspectedKani" />
+          <p v-else>Click on a node on the tree above to view its state.</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -43,11 +52,22 @@ onUnmounted(() => client.close());
 
 .left-container {
   height: 100%;
+  max-width: 50vw;
   padding: 4rem 4rem 2rem 4rem;
   background: bulmaRgba($beige-light, 0.2);
 }
 
 .chat-container {
   height: calc(100% - 3.5rem);
+}
+
+.right-container {
+  max-height: 100%;
+}
+
+.introspection-container {
+  padding: 0 2rem;
+  max-height: calc(100vh - (100vw / 4) - 2px);
+  overflow: scroll;
 }
 </style>
