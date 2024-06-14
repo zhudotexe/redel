c8cdb4289be9aaaac66231b742a5439b32183484
Andrew Zhu <me@andrew-zhu.com>
2024-06-14 14:36:44 -0400

    Update fanout-eval.sh

--- slurm/fanout-eval.sh
+++ slurm/fanout-eval.sh
@@ -10,4 +10,6 @@
 #SBATCH --mem=32G
 
 source slurm/fanout-eval-env.sh
-srun python score_fanoutqa.py experiments/fanoutqa/test/**/results.jsonl
+for i in experiments/fanoutqa/test/**/results.jsonl; do
+  srun python score_fanoutqa.py $i
+done
