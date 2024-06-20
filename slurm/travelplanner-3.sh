ad57eba5bd85b8ef82627b7cdb1adc8e9f0c6c70
Andrew Zhu <me@andrew-zhu.com>
2024-06-20 11:29:55 -0700

    exp: add travelplanner-3 slurm

---
+++ slurm/travelplanner-3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --partition=p_nlp
+#SBATCH --job-name=rd-tp-3
+#
+#SBATCH --output=/nlpgpu/data/andrz/logs/%j.%x.log
+#SBATCH --error=/nlpgpu/data/andrz/logs/%j.%x.log
+#SBATCH --time=7-0
+#SBATCH -c 1
+#SBATCH --mem=32G
+#SBATCH --mail-user=andrz@seas.upenn.edu
+#SBATCH --mail-type=END
+
+source slurm/env.sh
+srun python bench_travelplanner.py full
+srun python bench_travelplanner.py root-fc
+srun python bench_travelplanner.py baseline
