f6951937d34927c6c7b1a45a02864f132a98d49a
Andrew Zhu <me@andrew-zhu.com>
2024-07-08 16:18:38 -0700

    score: webarena AC/UA

--- score_webarena.py
+++ score_webarena.py
@@ -4,22 +4,61 @@ Usage: python score_webarena.py [path/to/results.jsonl,...]
 
 import asyncio
 import glob
+import json
 import sys
 from pathlib import Path
 
 from redel.utils import read_jsonl
 
-EXPECTED = 271
+WA_ROOT = Path(__file__).parent / "experiments/webarena"
+START_IDX = 0
+END_IDX = 812
+SKIP = 3  # 0, 812, 3 = 270 trials for small
+EXPECTED = len(range(START_IDX, END_IDX, SKIP))
+
+
+def get_config(idx: int) -> dict:
+    fp = WA_ROOT / f"config/{idx}.json"
+    with open(fp) as f:
+        return json.load(f)
 
 
 def score_submission(fp: Path):
     """Read in the results file and return the proportion of successful runs."""
+    # read results
     results = list(read_jsonl(fp))
+    # read configs, get achievable/unachievable idxs
+    achievable_idxs = set()
+    unachievable_idxs = set()
+    for idx in range(START_IDX, END_IDX, SKIP):
+        cfg = get_config(idx)
+        ra = cfg["eval"].get("reference_answers")
+        if ra and ra.get("fuzzy_match") == "N/A":
+            unachievable_idxs.add(idx)
+        else:
+            achievable_idxs.add(idx)
+
+    # total up results
     split = fp.parent.name
-    total = sum(r["score"] for r in results)
+    total = 0
+    total_achievable = 0
+    total_unachievable = 0
+    for r in results:
+        total += r["score"]
+        if r["id"] in achievable_idxs:
+            total_achievable += r["score"]
+        else:
+            total_unachievable += r["score"]
+
+    # print score
     n_success = len(results)
     score = total / EXPECTED
-    print(f"{split}: {score} -- {int(total)} / {EXPECTED} ({n_success} / {EXPECTED} result rows)")
+    score_achievable = total_achievable / len(achievable_idxs)
+    score_unachievable = total_unachievable / len(unachievable_idxs)
+    print(
+        f"{split}: {score} -- {int(total)} / {EXPECTED} ({n_success} / {EXPECTED} result rows) --"
+        f" {score_achievable} AC; {score_unachievable} UA"
+    )
 
 
 async def main():
