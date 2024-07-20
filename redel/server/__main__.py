df70acf09a51a87f3845e6ef258e6f4cdf2b18a1
Andrew Zhu <me@andrew-zhu.com>
2024-07-19 18:56:16 -0700

    docs: web interface

--- redel/server/__main__.py
+++ redel/server/__main__.py
@@ -14,10 +14,17 @@ log = logging.getLogger("redel-server")
 # todo args for save dirs
 
 # Define the configuration for each interactive session
-redel_config = dict(title=AUTOGENERATE_TITLE)
+redel_config = dict(
+    title=AUTOGENERATE_TITLE,
+    delegation_scheme=None,
+)
 
-log.info("Launching a minimal ReDel server with web browsing...")
-log.info("Please open the URL below in your favorite web browser.")
+print("Launching a minimal ReDel server with web browsing.")
+print()
+print("This is useful for quickly looking at replays, but the interactive system won't do much by default!")
+print("To configure an interactive system with tools, see https://redel.readthedocs.io/en/latest/viz.html")
+print()
+print("Please open the URL below in your favorite web browser.")
 
 # configure and start the server
 server = VizServer(redel_kwargs=redel_config)
