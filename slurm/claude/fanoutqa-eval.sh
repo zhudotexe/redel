2fa7fbe723959eadc3fa63803e4650be9089d444
Andrew Zhu <me@andrew-zhu.com>
2024-11-13 09:06:35 -0500

    add claude foqa eval

---
+++ slurm/claude/fanoutqa-eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --partition=p_nlp
+#SBATCH --job-name=rd-foqa-eval
+#
+#SBATCH --output=/nlpgpu/data/andrz/logs/%j.%x.log
+#SBATCH --error=/nlpgpu/data/andrz/logs/%j.%x.log
+#SBATCH --time=7-0
+#SBATCH -c 1
+#SBATCH --gpus=1
+#SBATCH --mem=32G
+
+source slurm/fanout-eval-env.sh
+srun python score_fanoutqa.py experiments/fanoutqa/claude/**/results.jsonl
