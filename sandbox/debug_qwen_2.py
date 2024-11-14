bb5451c49afda8ac3a2c323bd3d42952731d4cd7
Andrew Zhu <me@andrew-zhu.com>
2024-11-14 16:15:48 -0500

    Update debug_qwen_2.py

--- sandbox/debug_qwen_2.py
+++ sandbox/debug_qwen_2.py
@@ -14,8 +14,12 @@ model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", dev
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 model_inputs = tokenizer([prompt], return_tensors="pt").to(model.device)
+print(model_inputs)
 
 generated_ids = model.generate(**model_inputs, max_new_tokens=512)
 generated_ids = [output_ids[len(input_ids) :] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
+print(generated_ids)
 
 response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(prompt)
+print(response)
