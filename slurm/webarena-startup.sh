315cfadb9226972665dbbbc292551e443de92a41
Andrew Zhu <me@andrew-zhu.com>
2024-12-11 12:40:06 -0500

    chore: update webarena startup script

--- slurm/webarena-startup.sh
+++ slurm/webarena-startup.sh
@@ -1,6 +1,7 @@
 WA_SCRIPTS_BASE="/nlpgpu/data/andrz/webarena-setup/webarena"
 pushd $WA_SCRIPTS_BASE
 
+bash 01_docker_load_images.sh
 bash 02_docker_remove_containers.sh
 bash 03_docker_create_containers.sh
 bash 04_docker_start_containers.sh
